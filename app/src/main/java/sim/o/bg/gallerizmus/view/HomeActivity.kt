126ed826188ac3cdfb232db67c6cc6e728f563fa
simo-andreev <simeon.zlatanov.andreev@abv.bg>
2018-06-13 04:45:48 +0300

    [0000005] Add a Main activity

---
+++ app/src/main/java/sim/o/bg/gallerizmus/view/HomeActivity.kt
@@ -0,0 +1,14 @@
+package sim.o.bg.gallerizmus.view
+
+import android.app.Activity
+import android.os.Bundle
+import sim.o.bg.gallerizmus.R
+
+class HomeActivity : Activity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+}
