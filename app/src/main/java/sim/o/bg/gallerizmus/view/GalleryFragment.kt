0a24eae4994c418d09ad3ee44d064d586993cb92
simo-andreev <simeon.zlatanov.andreev@abv.bg>
2018-06-14 19:40:00 +0300

    [0000005] - It actuaklly loads images now! It's unusably slow, but does load images!

--- app/src/main/java/sim/o/bg/gallerizmus/view/GalleryFragment.kt
+++ app/src/main/java/sim/o/bg/gallerizmus/view/GalleryFragment.kt
@@ -2,6 +2,8 @@ package sim.o.bg.gallerizmus.view
 
 import android.app.Fragment
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
@@ -15,55 +17,64 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.Button
 import android.widget.ImageView
 
 
 class GalleryFragment : Fragment() {
+
+    private val adapter = GalleryRecyclerAdapter()
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view = RecyclerView(context)
         view.layoutParams = ViewGroup.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
         view.layoutManager = StaggeredGridLayoutManager(getColumnCount(), VERTICAL)
 
-        view.adapter = GalleryRecyclerAdapter()
+        view.adapter = adapter
         return view
     }
-
-
 
     private fun getColumnCount(): Int {
         val displayMetrics = context.resources.displayMetrics
         val dpWidth = displayMetrics.widthPixels / displayMetrics.density // display width
         return Math.max(1, (dpWidth / 160).toInt())
     }
+
+    fun setItems(queryCursors: MutableList<String>) {
+        adapter.items = queryCursors
+    }
 }
 
 
 class GalleryRecyclerAdapter : RecyclerView.Adapter<GalleryRecyclerAdapter.ImageHolder>(){
+    var items : List<String>? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageHolder {
         return ImageHolder(parent.context)
     }
 
     override fun getItemCount(): Int {
-        return 200
+        return Math.min(50, items!!.size)
     }
 
     override fun onBindViewHolder(holder: ImageHolder, position: Int) {
-        holder.bind(getItem(position))
+        holder.bind(getItem(position)!!)
     }
 
-    private fun getItem(position: Int): Drawable {
-        return ColorDrawable(Color.rgb(position, 1, 1))
+    private fun getItem(position: Int): Bitmap? {
+        val options = BitmapFactory.Options()
+        options.inSampleSize = 10
+        return BitmapFactory.decodeFile(items!![position], options)
     }
-
 
     class ImageHolder(context: Context) : RecyclerView.ViewHolder(ImageView(context)){
-        fun bind(item: Drawable) {
+        fun bind(item: Bitmap) {
             itemView as ImageView
-            val layoutParams = RecyclerView.LayoutParams(MATCH_PARENT, 100)
-            layoutParams.setMargins(10, 10, 10, 10)
+            val layoutParams = RecyclerView.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
+            layoutParams.setMargins(5, 5, 5, 5)
             itemView.layoutParams = layoutParams
-            itemView.setImageDrawable(item)
+            itemView.elevation = 8f
+            itemView.setImageBitmap(item)
         }
     }
 }
--- app/src/main/java/sim/o/bg/gallerizmus/view/HomeActivity.kt
+++ app/src/main/java/sim/o/bg/gallerizmus/view/HomeActivity.kt
@@ -1,25 +1,29 @@
 package sim.o.bg.gallerizmus.view
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.app.Activity
-import android.app.AlertDialog
 import android.content.Context
-import android.content.Intent
-import android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION
-import android.content.Intent.FLAG_GRANT_WRITE_URI_PERMISSION
-import android.content.UriPermission
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.database.Cursor
+import android.database.MergeCursor
 import android.os.Bundle
-import android.os.Environment
-import android.os.storage.StorageManager
-import android.os.storage.StorageVolume
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.toolbar.*
+import org.json.JSONArray
+import org.json.JSONObject
 import sim.o.bg.gallerizmus.R
-import java.io.File
+import kotlin.concurrent.timer
 
-class HomeActivity : Activity(){
 
-    private val REQUEST_CODE = 0b0000_1010
+private const val REQUEST_CODE = 0b0000_0001
+private const val READ_REQUEST_CODE = 0b0000_0010
+private const val REQUEST_CODE_GET_PERMISSIONS = 0b0000_0011
+
+class HomeActivity : Activity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,44 +31,75 @@ class HomeActivity : Activity(){
 
         setActionBar(toolbar)
 
-        fragmentManager.beginTransaction().add(R.id.homeScreenFragmentContainer, GalleryFragment()).commit()
+        val fragment = GalleryFragment()
+        fragment.setItems(queryCursors())
+        fragmentManager.beginTransaction().add(R.id.homeScreenFragmentContainer, fragment).commit()
 
-        requestImageDirAccess()
-    }
-
-    private fun requestImageDirAccess() {
-        val sm = getSystemService(Context.STORAGE_SERVICE) as StorageManager
-        val volume: StorageVolume = sm.primaryStorageVolume
-        volume.createAccessIntent(Environment.DIRECTORY_PICTURES).also { intent ->
-            startActivityForResult(intent, REQUEST_CODE)
-        }
-    }
-
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_CODE){
-
-            if (resultCode != RESULT_OK){
-                showAccessDenied()
-            } else {
-                data!!
-                val uri = data.data
-                if (!contentResolver.persistedUriPermissions.any { it.uri == uri }){
-                    contentResolver.takePersistableUriPermission(data.data, FLAG_GRANT_WRITE_URI_PERMISSION)
-                    Toast.makeText(this, "ADDING PURMMISHNZ!", 0).show()
-                }
-
-                Toast.makeText(this, data.data.path, 0).show()
+        val missingPermissions = mutableListOf<String>()
+        arrayOf(WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE).forEach {
+            if (checkSelfPermission(it) != PERMISSION_GRANTED) {
+                missingPermissions.add(it)
             }
         }
     }
 
-    private fun showAccessDenied() {
-        AlertDialog.Builder(this)
-                .setTitle(R.string.image_dir_access_denied_title)
-                .setMessage(R.string.image_dir_access_denied_message)
-                .setOnDismissListener { requestImageDirAccess() }
-                .create()
-                .show()
+    private fun queryCursors() : MutableList<String> {
+        val internalUri = MediaStore.Images.Media.INTERNAL_CONTENT_URI
+        val externalUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+
+        val projection = arrayOf(
+                MediaStore.MediaColumns.DATA
+        )
+
+        // You eyeing dem oddly placed ')'s? Written that way, the query compiles to:
+        // SELECT [...] (is_drm=0 OR is_drm IS NULL)) AND (_data IS NOT NULL) GROUP BY (bucket_display_name) | rather than:
+        // SELECT [...] (is_drm=0 OR is_drm IS NULL)) AND (_data IS NOT NULL  GROUP BY  bucket_display_name)
+        val internalCursor = contentResolver.query(internalUri, projection, null, null, null)
+        val externalCursor = contentResolver.query(externalUri, projection, null, null, null)
+        val mergedCursor = MergeCursor(arrayOf(internalCursor, externalCursor))
+
+        var albums = mutableListOf<String>()
+        val colPath = mergedCursor.getColumnIndex(MediaStore.MediaColumns.DATA)
+        while (mergedCursor.moveToNext()) {
+            albums.add(mergedCursor.getString(colPath))
+        }
+
+//        log("result", JSONObject(albums).toString())
+        mergedCursor.close()
+
+//        return System.currentTimeMillis() - start
+        return albums
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>?, grantResults: IntArray?) {
+        for ((index, permission) in permissions!!.withIndex()) {
+            toast("$permission : ${grantResults!![index] == RESULT_OK}", true)
+        }
+
+        queryCursors()
+    }
+
+    private fun log(tag: String, text: String) {
+        Log.wtf("TAAAG", text)
+    }
+
+
+    fun getCount(c: Context, album_name: String): String {
+        val uriExternal = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        val uriInternal = android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI
+
+        val projection = arrayOf(MediaStore.MediaColumns.DATA, MediaStore.Images.Media.BUCKET_DISPLAY_NAME, MediaStore.MediaColumns.DATE_MODIFIED)
+        val cursorExternal = c.contentResolver.query(uriExternal, projection, "bucket_display_name = \"$album_name\"", null, null)
+        val cursorInternal = c.contentResolver.query(uriInternal, projection, "bucket_display_name = \"$album_name\"", null, null)
+        val cursor = MergeCursor(arrayOf<Cursor>(cursorExternal, cursorInternal))
+
+
+        return cursor.count.toString() + " Photos"
+    }
+
+}
+
+fun Activity.toast(text: String, log: Boolean = false) {
+    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+    if (log) Log.wtf("TAAAG", text)
 }
